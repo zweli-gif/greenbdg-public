1cdd95001ed55669db0686474582a2ed8618cf87
zweli-gif <zweli@gwebityala.com>
2026-03-27 21:44:15 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -153,6 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
+    base: '/greenbdg-public/',
   plugins,
   resolve: {
     alias: {
